8b45492f4aa183648c64f3afc58a4969d73267bf
Bastien Nocera <hadess@hadess.net>
2026-05-26 15:26:12 +0200

    Convert manifest to YAML
    and adapt build script for new manifest path

--- local-build.sh
+++ local-build.sh
@@ -4,6 +4,6 @@ rm -f com.retrodev.blastem.flatpak
 rm -rf _build ; mkdir _build
 rm -rf _repo ; mkdir _repo
 
-flatpak-builder --ccache --force-clean _build com.retrodev.blastem.json --repo=_repo
+flatpak-builder --ccache --force-clean _build com.retrodev.blastem.yaml --repo=_repo
 flatpak build-bundle _repo com.retrodev.blastem.flatpak com.retrodev.blastem master
 
